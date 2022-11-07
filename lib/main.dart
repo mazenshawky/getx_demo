9fe1f18e60106143e3e90cb2bb6dfca2126b9cf6
Mazen Shawky <mazenshawqy@gmail.com>
2022-11-07 19:53:12 +0200

    device language

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getx_demo/locale/locale.dart';
 import 'package:getx_demo/view/home.dart';
 
 void main() {
@@ -17,7 +18,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Home(),
+      locale: Get.deviceLocale,
+      translations: MyLocale(),
       initialRoute: "/",
       getPages: [
         GetPage(name: "/", page: () => const Home()),
--- lib/view/home.dart
+++ lib/view/home.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
-import 'package:getx_demo/controller/homecontroller.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -11,35 +10,30 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("1".tr),
+      ),
       body: Container(
         padding: const EdgeInsets.symmetric(horizontal: 10),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            GetBuilder<HomeController>(
-              init: HomeController(),
-              builder: (controller) => Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                    onPressed: (){
-                      controller.increment();
-                    },
-                    icon: const Icon(Icons.add, size: 30),
-                  ),
-                  Center(
-                    child: Text(
-                      "${controller.counter}",
-                      style: const TextStyle(fontSize: 20),
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: (){
-                      controller.decrement();
-                    },
-                    icon: const Icon(Icons.remove, size: 30),
-                  ),
-                ],
+            Center(
+              child: MaterialButton(
+                onPressed: (){
+                },
+                color: Colors.red,
+                textColor: Colors.white,
+                child: Text('2'.tr),
+              ),
+            ),
+            Center(
+              child: MaterialButton(
+                onPressed: (){
+                },
+                color: Colors.red,
+                textColor: Colors.white,
+                child: Text('3'.tr),
               ),
             ),
           ],
